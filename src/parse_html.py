253031562986a117c5680cbf7ef57a18ebc36c86
88VwV88 <dhimankkvivek773@gmail.com>
2024-05-06 14:28:51 +0530

    Update the regex to parse multiple tag attributes and use python file handling to read file contents

--- src/parse_html.py
+++ src/parse_html.py
@@ -1,21 +1,32 @@
 import re
 
+SPLIT_TAGS = re.compile(r'''
+                    <(\w+)(\s*([\w\-=\.#\\/'"]*))>
+                        ([^\1]*)
+                    </\1>
+                        ''', re.VERBOSE)
 PARSE_TAG = re.compile(r'''
-                    <(?P<tag>\w+)\s*(?P<attribs>[\w\-=\.#\\/'"]*)>
+                    <(?P<name>\w+)(?P<attribs>[\s\w\-=\.:#\\/'"]*)>
                         (?P<content>[^\1]*)
                     </\1>
                        ''', re.VERBOSE)
 
 def parse_html(html):
-
-    if (matches := PARSE_TAG.split(html)):
-        DOM = []
-        for match in matches:
-            print(f'{match = }')
-            # tag, attribs, contents = match.groups()
-            # DOM += {tag: [attribs, parse_html(contents) or contents]}
+    if parts := PARSE_TAG.findall(html):
+        DOM = {}
+        for name, attribs, content in parts:
+            DOM[name] = (attribs.strip(), parse_html(content))
         return DOM
     else:
         return ''
 
-print(parse_html('<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" href="#"></head><body class="main"><p>Hello, World!</p></body></html>'))
+def read_html(filename: str) -> str | None:
+    contents = b""
+    with open(filename, "rb") as file:
+        for line in file:
+            contents += line.strip()
+    return contents.decode(encoding='utf-8')
+
+if __name__ == '__main__':
+    _html = read_html('career-synopsis.html')
+    print(parse_html(_html))
